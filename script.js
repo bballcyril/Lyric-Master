8fe3e914f86ef51058103a1993c1b255fa0a9ddb
Cyril Maandal <maandalc@yahoo.com>
2019-04-10 22:18:14 -0400

    added comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var request = new XMLHttpRequest();
+var lyrics = document.getElementById("lyrics");
 var data;
 
-
+//Gets the JSON data from the lyrics.ovh api
 request.open("GET","https://api.lyrics.ovh/v1/Drake/One_dance");
 request.addEventListener("load",whenLoaded);
 request.send();
@@ -9,7 +10,6 @@ request.send();
 function whenLoaded(){
   //data = JSON.parse(request.responseText);
   data = request.responseText;
-  var lyrics = document.getElementById("lyrics");
   var output = format(data);
   lyrics.innerHTML += output;
   console.log(data);
@@ -17,16 +17,20 @@ function whenLoaded(){
 //Format the JSON data
 function format(data){
   var formatted =" ";
+  //Position of the start of the lyrics
   var pos = data.indexOf(":")+1;
+  //Index of the next \n
   var nextLinePos = data.indexOf("\\n",pos);
-
+  //Iterate through each character
   for(var i=pos;i<data.length-1;i++){
     if(nextLinePos===i){
+      //Add a break into the html if i is the start of \n
       formatted+= "<br>";
       i+=1;
     }else{
     formatted+= data.substring(i,i+1);
     }
+    //Gets the next index of \n
     nextLinePos = data.indexOf("\\n",i);
   }
   return formatted;
